cd850f64bcd90663fb2ddc6a309422c4e13603ff
Raine Petersen <raineforest01@gmail.com>
2022-03-28 22:39:30 -0600

    Added a few new methods and updated the README

--- README.md
+++ README.md
@@ -1,19 +1,95 @@
 # Lambda Proxy HTTP Handler
 
-This library for handling http requests within the context of a lambda handler function. If you're familiar with express.js you will feel right at home working with this library.
+This library is for handling http requests within the context of a lambda function handler method. If you're familiar with express.js you will feel right at home working with lambda-http-handler.
 
 ## Getting started
 
-    npm install @raine-works/lambda-http-handler 
-    
+`EXAMPLE`
+
+    npm install @raine-works/lambda-http-handler
+
     const { HTTP } = require('@raine-works/lambda-http-handler')
-    
+
     export const handler = async (event, context, callback) => {
-        const http = HTTP(event, callback, context)
-        
-        http.method('GET', '/route/:id', (event) => {
-            http.status(200).headers({ content-type: 'application/json' }).body('Hi mom!').send()    
+        try {
+            const http = new HTTP(event, callback, context)
+
+            http.method('GET', '/route/:id', (event) => {
+                http.status(200).headers({ content-type: 'application/json' }).body('Hi mom!').send()
+            }
+
+            http._404()
+        } catch (error) {
+            http._500(error.message)
         }
-        
-        http._404()
     }
+
+## API reference
+
+`Create a new HTTP instance`
+
+The HTTP class requires a lambda event and callback; the context object is optional.
+
+    const http = new HTTP(event, callback, context)
+
+`method`
+
+The method function captures the http method and path and then passes the event object to a callback function if they match the event.
+
+    http.method('POST', '/create-user', (event) => {
+        // Your code here
+    })
+
+The method function also parses the event body and captures path parameters. To use custom parameters you can use the following syntax.
+
+    http.method('GET', '/user/:id', (event) => {
+        // Your code here
+    })
+
+`status`
+
+The status function sets the status code on the lambda response object.
+
+    http.status(418)
+
+`headers`
+
+The headers function sets the headers on the lambda response object.
+
+    http.headers({ 'content-type': 'application/json' })
+
+`body`
+
+The body function sets the body content on the lambda response object.
+
+    http.body('Hello World!')
+
+`send`
+
+The send function executes the lambda callback method and passed the lambda response object. This should be used after setting at least the status and body.
+
+    http.status(200).body('Hi Mom!').send()
+
+`_200`
+
+The \_200 function is a quick way to send a string back to the client with a 200 status code.
+
+    http._200('It worked...')
+
+`_400`
+
+The \_400 function is a quick way to send an error message back to the client with a 400 status code.
+
+    http._400('Bad request...')
+
+`_404`
+
+The \_404 function is quick way to send a 404 status code back to the client. This method automatically sets the body content to "The requested resource was not found..."
+
+    http._404()
+
+`_500`
+
+The \_500 function is a quick way to send an error message back to the client with a 500 status code.
+
+    http._500('Internal server error...')
--- src/index.ts
+++ src/index.ts
@@ -13,32 +13,72 @@ export class HTTP {
     }
     constructor(
         event: APIGatewayProxyEvent,
-        context: Context, 
-        callback: APIGatewayProxyCallback
+        callback: APIGatewayProxyCallback,
+        context: Context
     ) {
         global.stage = event.requestContext.stage
         this.event = event
-        this.context = context
         this.callback = callback
+        this.context = context
     }
     
     // RESPONSE CONFIGS
 
+    /**
+     * Set status code
+     * @param val {number}
+     * @returns 
+     */
     status(val: number) {
         this.response.statusCode = val
         return this
     }
 
+    /**
+     * Set headers
+     * @param val {object}
+     * @returns 
+     */
     headers(val: any) {
         this.response.headers = val
         return this
     }
 
+    /**
+     * Set body content
+     * @param val {string}
+     * @returns 
+     */
     body(val: string) {
         this.response.body = val
         return this
     }
 
+    /**
+     * Send quick 200 response
+     * @param val {string}
+     */
+    _200(val: string) {
+        this.callback(null, {
+            statusCode: 200, 
+            body: val
+        })
+    }
+
+    /**
+     * Send quick 400 response
+     * @param error {string}
+     */
+    _400(error: string) {
+        this.callback(null, {
+            statusCode: 400, 
+            body: error
+        })
+    }
+
+    /**
+     * Send quick 404 response
+     */
     _404() {
         this.callback(null, {
             statusCode: 404, 
@@ -46,6 +86,10 @@ export class HTTP {
         })
     }
 
+    /**
+     * Send quick 500 response
+     * @param error {string}
+     */
     _500(error: string) {
         this.callback(null, {
             statusCode: 500, 
@@ -53,14 +97,25 @@ export class HTTP {
         })
     }
 
+    /**
+     * Send response after after setting the status, headers, and body
+     */
     send() {
         this.callback(null, this.response)
     }
 
     // HTTP METHODS
 
+    /**
+     * Define method and path
+     * @param method 
+     * @param path 
+     * @param callback 
+     * @returns 
+     */
     method(method: string, path: string, callback: Function) {
         if (pathResolver(method, path, this.event)) {
+            bodyParser(this.event)
             callback(this.event)
         } else {
             return this
@@ -71,6 +126,14 @@ export class HTTP {
 interface Params {
     [key: string]: string
 }
+
+/**
+ * Resolve path and params
+ * @param method 
+ * @param path 
+ * @param event 
+ * @returns 
+ */
 const pathResolver = (method: string, path: string, event: APIGatewayProxyEvent): boolean => {
     let params: Params = {}
     let pathValues = event.path.split('/').filter((e) => { return e !== '' })
@@ -79,7 +142,7 @@ const pathResolver = (method: string, path: string, event: APIGatewayProxyEvent)
     for (let i = pathValues.length - paramKeys.length; i < pathValues.length; i++) {
         params[paramKeys[i]?.split(':')[1]] = pathValues[i]
     }
-    let fullRootPath = paramKeys.length > 0 ? `${rootPath}/${paramKeys.join('/')}` : rootPath
+    let fullRootPath = paramKeys.length > 0 ? rootPath === '/' ? `${rootPath}${paramKeys.join('/')}` : `${rootPath}/${paramKeys.join('/')}` : rootPath
     event.pathParameters = params
     if (
         method.toUpperCase() === event.httpMethod 
@@ -89,6 +152,14 @@ const pathResolver = (method: string, path: string, event: APIGatewayProxyEvent)
     } else {
         return false
     }
+}
+
+/**
+ * Parse body content
+ * @param event 
+ */
+const bodyParser = (event: APIGatewayProxyEvent) => {
+    event.body = event.body ? JSON.parse(event.body) : {}
 }
 
 export type { APIGatewayProxyCallback, APIGatewayProxyResult, APIGatewayProxyEvent, Context, APIGatewayProxyHandler }
